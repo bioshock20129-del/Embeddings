51fa7d2b8193aba728ef87e3fe560da5b5047ace
AlexRuMath <bioshock20129@gmail.com>
2025-10-25 14:52:22 +0300

    update commit

--- test.py
+++
@@ -1,8 +0,0 @@
-from pyan import create_callgraph
-
-options = {'colored': True, 'grouped': True}
-
-output = create_callgraph("agent.py", **options)
-
-with open('graph.dot', 'w') as f:
-    f.write(output)
